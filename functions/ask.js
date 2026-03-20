2cc5a932aa0da5e45e28755597b1e5e21061cf1d
clementeduranmoraga-jpg <clementeduranmoraga@gmail.com>
2026-03-19 21:51:06 -0300

    Update ask.js

--- functions/ask.js
+++ functions/ask.js
@@ -26,7 +26,7 @@ exports.handler = async (event) => {
   };
 
   try {
-    const model = "ggemini-1.5-flash-latest";
+    const model = "gemini-1.5-flash-latest";
     const url = `https://generativelanguage.googleapis.com/v1beta/models/${model}:generateContent?key=${API_KEY}`;
     
     const res = await fetch(url, {
